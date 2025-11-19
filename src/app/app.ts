2eeb3d385f3fe41f30c875d936c0d2ca92a0c632
Vmbundu <VandayBundu3@gmail.com>
2025-11-19 10:18:52 -0500

    Completed bootstrap styling

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Patient } from './patient/patient'
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Patient],
+  imports: [RouterOutlet, Patient, NgbModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
